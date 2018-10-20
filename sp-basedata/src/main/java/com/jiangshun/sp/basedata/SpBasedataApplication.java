7f04095983f2d9df3881bc3a4578a439b082aef0
jiangshun <l8r_127@126.com>
2018-10-20 15:36:29 +0800

    commit code

--- sp-basedata/src/main/java/com/jiangshun/sp/basedata/SpBasedataApplication.java
+++ sp-basedata/src/main/java/com/jiangshun/sp/basedata/SpBasedataApplication.java
@@ -1,12 +1,37 @@
 package com.jiangshun.sp.basedata;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+//@EnableEurekaServer
+@MapperScan("cn.jiafeng.service.usercenter.dao")
 public class SpBasedataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpBasedataApplication.class, args);
+	}
+	
+
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		return corsConfiguration;
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig()); // 4
+		return new CorsFilter(source);
 	}
 }
---
+++ sp-basedata/src/main/java/com/jiangshun/sp/basedata/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.jiangshun.sp.basedata.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jiangshun.sp.basedata.extend.BaseController;
+
+@RestController
+public class HomeController extends BaseController {
+
+	@RequestMapping("index")
+	public Object Index() {
+		return Func(() -> {
+			return "Hello world!";
+		});
+	}
+}
