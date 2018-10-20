4facb18dad6d57260bdcead9e78305d26cb8b277
jiangshun <l8r_127@126.com>
2018-10-20 14:09:15 +0800

    init basedata

---
+++ sp-basedata/src/main/java/com/jiangshun/sp/basedata/SpBasedataApplication.java
@@ -0,0 +1,12 @@
+package com.jiangshun.sp.basedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpBasedataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpBasedataApplication.class, args);
+	}
+}
